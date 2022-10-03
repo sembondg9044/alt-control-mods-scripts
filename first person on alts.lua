a7500c6c6bc3f1ccde820b763e9759b1478681d6
eee <101921237+sembondg9044@users.noreply.github.com>
2022-10-03 13:16:29 +0800

    Create first person on alts.lua

---
+++ first person on alts.lua
@@ -0,0 +1,155 @@
+
+local ID = game.Players.LocalPlayer.UserId
+local firstperson = game:GetService("Players").LocalPlayer.CameraMode = Enum.CameraMode.LockFirstPerson
+
+if ID == Alts['Alt1'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt2'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt3'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt4'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt5'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt6'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt7'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt8'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt9'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt10'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt11'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt12'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt13'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt14'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt15'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt16'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt17'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt18'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt19'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt20'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt21'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt22'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt23'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt24'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt25'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt826'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt27'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt28'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt29'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt30'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt31'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt32'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt33'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt34'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt35'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt36'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt37'] then
+    firstperson
+    end
+    
+    if ID == Alts['Alt38'] then
+    firstperson
+    end
