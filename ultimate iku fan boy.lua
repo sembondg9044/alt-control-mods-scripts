b399c9262fbafcab676226de0703df3c25818e8f
eee <101921237+sembondg9044@users.noreply.github.com>
2022-11-02 18:35:51 +0800

    Add files via upload

---
+++ ultimate iku fan boy.lua
@@ -0,0 +1,95 @@
+--fun fact, people who tries to read my code dies from cringe so please, do not read my coding😭😭😭😭😭😭😭😭😭😭
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+local ID = game.Players.LocalPlayer.UserId
+
+--I spammed the detection cuz it gave errors so dont blame me.
+
+
+
+
+
+--simple seller
+if ID == getgenv().Settings.host then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/W9K/roblox-scripts/main/DHC/SimpleSeller.lua', true))()
+end
+wait(2)
+--cash counter
+if ID == getgenv().Settings.host then
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/OG5/IkuScripts/main/DH/MoneyCounter', true))()
+end
+
+for i, v in pairs(game.Workspace:GetDescendants()) do
+    if v:IsA("Part") then
+        v.Material = Enum.Material.SmoothPlastic
+    end
+end  
+
+loadstring(game:HttpGet('https://raw.githubusercontent.com/W9K/roblox-scripts/main/fpsshit.lua', true))()
+
+settings().Physics.PhysicsEnvironmentalThrottle = 1
+settings().Rendering.QualityLevel = 'Level01'
+UserSettings():GetService('UserGameSettings').MasterVolume = 0
